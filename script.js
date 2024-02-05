cafbf904ddeb4d58f320ce42cccfae81da89f815
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-05 18:53:42 +0800

    added image rotate on scroll

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Test")
+const image = document.querySelector("img")
+let ROTATE_SPEED = 0
+window.addEventListener("wheel", e => {
+  console.log(e.deltaY == 100)
+  if(e.deltaY > 0) {
+    ROTATE_SPEED -= 10
+    image.style.rotate = `${ROTATE_SPEED}deg`
+  } else {
+    ROTATE_SPEED += 10
+    image.style.rotate = `${ROTATE_SPEED}deg`
+  }
+})
